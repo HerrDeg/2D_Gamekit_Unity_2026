eb61b6be14feb0e37ff443422c660c3c56ceb4e9
HerrDeg <98744008+HerrDeg@users.noreply.github.com>
2026-05-05 15:20:11 +0200

    Changed some starting settings and added Ambiance + Player Ranged sound

--- Assets/2DGamekit/Scripts/Audio/AudioManager.cs
+++ Assets/2DGamekit/Scripts/Audio/AudioManager.cs
@@ -15,6 +15,7 @@ public class AudioManager : MonoBehaviour
     [System.Serializable]
     public struct Emitters
     {
+        public StudioEventEmitter ambiance;
         public StudioEventEmitter musicMenu;
         public StudioEventEmitter music;
         public StudioEventEmitter music02;
@@ -31,6 +32,7 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private EventReference playerJump;
     [SerializeField] private EventReference playerLand;
     [SerializeField] private EventReference playerAttackMelee;
+    [SerializeField] private EventReference playerAttackRanged;
     
     [SerializeField] private EventReference playerHurt;
     EventInstance playerFootstepInstance;
@@ -226,7 +228,15 @@ public class AudioManager : MonoBehaviour
         RuntimeManager.PlayOneShot(playerAttackMelee, transform.position);
     }
 
-	
+    public void PlayRanged()
+    {
+        if (playerAttackRanged.IsNull)
+        {
+            Debug.LogWarning("Fmod event not found: playerAttackRanged");
+            return;
+        }
+        RuntimeManager.PlayOneShot(playerAttackRanged);
+    }
 
     public void PlayHurt()
     {
